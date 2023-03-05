f2f5b4472389f963d4e190f3fef44ccab5fc048c
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-03-05 14:19:16 +0800

    集成i18n

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+// import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+// import en from 'element-plus/lib/locale/lang/en'
+
+import { useI18n } from 'vue-i18n'
+const { locale: localeLanguage } = useI18n()
+// const changeLang = (language: any) => {
+//   localeLanguage.value = language.name
+//   console.log('language：', language)
+// }
 
 const router = useRouter()
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <el-config-provider :locale=localeLanguage>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+    </div>
 
-  <!-- 触发路由（router-view）切换方式1 -->
-  <button @click="() => router.push({path: '/home' })">首页</button>
-  <button @click="() => router.push({ path: '/mine'})">个人中心</button>
+    <!-- 触发路由（router-view）切换方式1 -->
+    <button @click="() => router.push({path: '/home' })">首页</button>
+    <button @click="() => router.push({ path: '/mine'})">个人中心</button>
 
-  <!-- 触发路由（router-view）切换方式2 -->
-  <router-link to="/home">首页</router-link>
-  <router-link to="/mine">个人中心</router-link>
+    <!-- 触发路由（router-view）切换方式2 -->
+    <router-link to="/home">首页</router-link>
+    <router-link to="/mine">个人中心</router-link>
 
-  <router-view />
-  <HelloWorld msg="Vite + Vue" />
+    <router-view />
+    <HelloWorld msg="Vite + Vue" />
+  </el-config-provider>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import App from './App.vue'
 import router from "./router";
 import ElementPlus, { ElMessage } from "element-plus";
 import "element-plus/dist/index.css";
+import { createSSRI18n } from './language/i18n'
 
 const app = createApp(App);
+const i18n = createSSRI18n()
 app.use(router)
 app.use(ElementPlus);
+app.use(i18n)
 app.config.globalProperties.$message = ElMessage;
 app.mount("#app");
